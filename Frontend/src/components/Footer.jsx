69b9f6428831acfb8183a2bd7b5ad26d918f9302
james <jimmindungu3@gmail.com>
2024-11-21 09:40:59 +0300

    add yotube icon to footer

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { MdOutlineEmail } from "react-icons/md";
 import { FaGithub } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa6";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaTiktok } from "react-icons/fa";
@@ -15,17 +16,18 @@ const Footer = () => {
         </div>
         <div className="flex space-x-4 text-gray-200 text-lg">
           <a
-            href="mailto:jimmindungu3@gmail.com"
-            className="text-xl hover:text-yellow-500 active:text-white transition-colors"
-          >
-            <MdOutlineEmail />
-          </a>
-          <a
             href="https://github.com/jimmindungu3"
             target="_blank"
             className="hover:text-yellow-500 active:text-white transition-colors"
           >
             <FaGithub />
+          </a>
+          <a
+            href="https://www.youtube.com/@DevsToday"
+            target="_blank"
+            className="hover:text-yellow-500 active:text-white transition-colors"
+          >
+            <FaYoutube />
           </a>
           <a
             href="https://www.linkedin.com/in/jamesndunguthedev/"
