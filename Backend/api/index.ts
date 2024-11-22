d65a1577feaf0a804fef13a68318b41cee6673cc
james <jimmindungu3@gmail.com>
2024-11-22 12:45:32 +0300

    config cors

--- Backend/api/index.ts
+++ Backend/api/index.ts
@@ -1,8 +1,10 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
+app.use(cors())
 const PORT = 8000;
 const password = process.env.GMAIL_APP_PASSWORD;
 
